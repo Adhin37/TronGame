0522b01fc30b1769a3e9b390089b9c2548ab5f0d
Adhin37 <thomasdemay37@gmail.com>
2018-07-15 13:09:49 +0200

    ide settings

--- src/Player.java
+++ src/Player.java
@@ -13,7 +13,7 @@ class LightRunner {
 	public LightRunner(final int id, final int beginningX, final int beginningY, final int currentX,
 			final int currentY) {
 		this.id = id;
-		this.movesPossible=0;
+		this.movesPossible = 0;
 		this.beginningX = beginningX;
 		this.beginningY = beginningY;
 		this.setCurrentX(currentX);
@@ -23,6 +23,7 @@ class LightRunner {
 	public int getId() {
 		return id;
 	}
+
 	public int getMovesPossible() {
 		return movesPossible;
 	}
@@ -30,7 +31,7 @@ class LightRunner {
 	public void setMovesPossible(final int movesPossible) {
 		this.movesPossible = movesPossible;
 	}
-	
+
 	public int getBeginningX() {
 		return beginningX;
 	}
@@ -125,54 +126,54 @@ class GameMap {
 		nearPositions.put(3, map[posY][posX + 1]); // Right
 		return nearPositions;
 	}
-	
-	public int calculMoves(int[][] map,final int playerCptY,final int playerCptX) {
-	    int maxMoves;
-	    maxMoves = 0;
-		if((playerCptY) >= 0 && (playerCptY) < this.getMaxY() && (playerCptX) >= 0 && (playerCptX) < this.getMaxX() && map[playerCptY][playerCptX] == -1){
-		    map[playerCptY][playerCptX] = -3;
-		    maxMoves += calculMoves(map,playerCptY,playerCptX)+1;
+
+	public int calculMoves(int[][] map, final int playerCptY, final int playerCptX) {
+		int maxMoves;
+		maxMoves = 0;
+		if ((playerCptY) >= 0 && (playerCptY) < this.getMaxY() && (playerCptX) >= 0 && (playerCptX) < this.getMaxX()
+				&& map[playerCptY][playerCptX] == -1) {
+			map[playerCptY][playerCptX] = -3;
+			maxMoves += calculMoves(map, playerCptY, playerCptX) + 1;
 		}
-		if((playerCptY-1) >= 0 && map[playerCptY-1][playerCptX] == -1){
-		    map[playerCptY-1][playerCptX] = -3;
-		    maxMoves += calculMoves(map,playerCptY-1,playerCptX)+1;
+		if ((playerCptY - 1) >= 0 && map[playerCptY - 1][playerCptX] == -1) {
+			map[playerCptY - 1][playerCptX] = -3;
+			maxMoves += calculMoves(map, playerCptY - 1, playerCptX) + 1;
 		}
-		if((playerCptY+1) < this.getMaxY() && map[playerCptY+1][playerCptX] == -1){
-		    map[playerCptY+1][playerCptX] = -3;
-		    maxMoves += calculMoves(map,playerCptY,playerCptX)+1;
+		if ((playerCptY + 1) < this.getMaxY() && map[playerCptY + 1][playerCptX] == -1) {
+			map[playerCptY + 1][playerCptX] = -3;
+			maxMoves += calculMoves(map, playerCptY, playerCptX) + 1;
 		}
-		if((playerCptX-1) >= 0 && map[playerCptY][playerCptX-1] == -1){
-		    map[playerCptY][playerCptX-1] = -3;
-		    maxMoves += calculMoves(map,playerCptY,playerCptX-1)+1;
+		if ((playerCptX - 1) >= 0 && map[playerCptY][playerCptX - 1] == -1) {
+			map[playerCptY][playerCptX - 1] = -3;
+			maxMoves += calculMoves(map, playerCptY, playerCptX - 1) + 1;
 		}
-		if((playerCptX+1) < this.getMaxX() && map[playerCptY][playerCptX+1] == -1){
-		    map[playerCptY][playerCptX+1] = -3;
-		    maxMoves += calculMoves(map,playerCptY,playerCptX+1)+1;
+		if ((playerCptX + 1) < this.getMaxX() && map[playerCptY][playerCptX + 1] == -1) {
+			map[playerCptY][playerCptX + 1] = -3;
+			maxMoves += calculMoves(map, playerCptY, playerCptX + 1) + 1;
 		}
-	    return maxMoves;
+		return maxMoves;
 	}
-	
+
 	private void iniMap() {
 		for (int cptY = 0; cptY < this.maxY; cptY++) {
 			for (int cptX = 0; cptX < this.maxX; cptX++) {
-				if(cptY==0 || cptY+1 == this.maxY || cptX==0 || cptX+1 == this.maxX){
-				    map[cptY][cptX] = -2; // Limit map
-				}
-				else{
-				   map[cptY][cptX] = -1; // No player
+				if (cptY == 0 || cptY + 1 == this.maxY || cptX == 0 || cptX + 1 == this.maxX) {
+					map[cptY][cptX] = -2; // Limit map
+				} else {
+					map[cptY][cptX] = -1; // No player
 				}
 			}
 		}
 	}
-    
-    public int[][] multiArrayCopy(int[][] source){
-        int[][] destination = new int[source[0].length][source[1].length];
-    	for (int i=0;i<source.length;i++){
-    		destination[i] = source[i].clone();
-    	}
-    	return destination;
-    }
-    
+
+	public int[][] multiArrayCopy(int[][] source) {
+		int[][] destination = new int[source[0].length][source[1].length];
+		for (int i = 0; i < source.length; i++) {
+			destination[i] = source[i].clone();
+		}
+		return destination;
+	}
+
 	public int getMaxX() {
 		return maxY;
 	}
@@ -186,8 +187,8 @@ class GameMap {
 class GameOrder {
 	private GameMap tronMap;
 	private int myLightRunnerID;
-    private ArrayList<LightRunner> allPlayers;
-    
+	private ArrayList<LightRunner> allPlayers;
+
 	public GameOrder() {
 	}
 
@@ -200,103 +201,107 @@ class GameOrder {
 	public String makeBestMove() {
 		return this.followOrder(this.calcBestMove(this.myLightRunnerID));
 	}
-	
-    private int score(){
-        int score=0;
-        // if (game.win?(@player)){
-        //     score= 10;
-        // }
-        // else if (game.win?(@opponent)){
-        //     score= -10;
-        // }
-        return score;
-    }
 
-    private int predictFuture(final ArrayList<Integer> bestMoveArray, final int currentY, final int currentX){
-        int bestFuture;
-        bestFuture = -1;
-        if(bestMoveArray.isEmpty()){
-            return bestFuture;
-        }
-        int[] possibilities;
-        int up, down, left, right;
-        int max;
-        up = 0; down = 0; left = 0; right = 0;
-        max = 0;
-        if (bestMoveArray.contains(0)) {
-            up = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()),currentY-1,currentX);
-        } else{
-            up=0;
-        }       
-        if (bestMoveArray.contains(1)) {
-            down = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()),currentY+1,currentX);
-        } else{
-            down=0;
-        } 
-        if (bestMoveArray.contains(2)) {
-            left = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()),currentY,currentX-1);
-        } else{
-            left=0;
-        } 
-        if (bestMoveArray.contains(3)) {
-            right = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()),currentY,currentX+1);
-        } else{
-            right=0;
-        } 
-        
-        possibilities = new int[] {up, down, left, right};
-        for (int counter = 0; counter < possibilities.length; counter++) {
-            if (possibilities[counter] > max) {
-                System.err.println("possibilities:"+possibilities[counter]);
-                max = possibilities[counter];
-                bestFuture=counter;
-            }
-        }
-        return bestFuture;
-    }
-    
+	private int score() {
+		int score = 0;
+		// if (game.win?(@player)){
+		// score= 10;
+		// }
+		// else if (game.win?(@opponent)){
+		// score= -10;
+		// }
+		return score;
+	}
+
+	private int predictFuture(final ArrayList<Integer> bestMoveArray, final int currentY, final int currentX) {
+		int bestFuture;
+		bestFuture = -1;
+		if (bestMoveArray.isEmpty()) {
+			return bestFuture;
+		}
+		int[] possibilities;
+		int up, down, left, right;
+		int max;
+		up = 0;
+		down = 0;
+		left = 0;
+		right = 0;
+		max = 0;
+		if (bestMoveArray.contains(0)) {
+			up = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()), currentY - 1, currentX);
+		} else {
+			up = 0;
+		}
+		if (bestMoveArray.contains(1)) {
+			down = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()), currentY + 1, currentX);
+		} else {
+			down = 0;
+		}
+		if (bestMoveArray.contains(2)) {
+			left = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()), currentY, currentX - 1);
+		} else {
+			left = 0;
+		}
+		if (bestMoveArray.contains(3)) {
+			right = tronMap.calculMoves(tronMap.multiArrayCopy(tronMap.getMap()), currentY, currentX + 1);
+		} else {
+			right = 0;
+		}
+
+		possibilities = new int[] { up, down, left, right };
+		for (int counter = 0; counter < possibilities.length; counter++) {
+			if (possibilities[counter] > max) {
+				System.err.println("possibilities:" + possibilities[counter]);
+				max = possibilities[counter];
+				bestFuture = counter;
+			}
+		}
+		return bestFuture;
+	}
+
 	private int calcBestMove(int lightRunnerID) {
 		int bestMove;
 		int currentY, currentX;
 		int sizeArray;
 		bestMove = -1;
-		currentY = 0; currentX = 0;
+		currentY = 0;
+		currentX = 0;
 		sizeArray = 0;
-		
+
 		HashMap<Integer, Integer> nearPositions;
 		ArrayList<Integer> bestMoveArray = new ArrayList<Integer>();
-		currentY=this.allPlayers.get(lightRunnerID).getCurrentY();
-		currentX=this.allPlayers.get(lightRunnerID).getCurrentX();
-		nearPositions = this.tronMap.whatsNear(currentY,currentX);
+		currentY = this.allPlayers.get(lightRunnerID).getCurrentY();
+		currentX = this.allPlayers.get(lightRunnerID).getCurrentX();
+		nearPositions = this.tronMap.whatsNear(currentY, currentX);
 		sizeArray = nearPositions.size();
-		
-		for (int cpt = sizeArray-1; cpt >= 0; cpt--) {
+
+		for (int cpt = sizeArray - 1; cpt >= 0; cpt--) {
 			if (nearPositions.get(cpt) == -1) {
 				bestMoveArray.add(cpt);
 			}
 		}
-		for (int move : bestMoveArray){
-		    System.err.println("array:"+move);
+		for (int move : bestMoveArray) {
+			System.err.println("array:" + move);
 		}
-		bestMove = predictFuture(bestMoveArray,currentY,currentX);
+		bestMove = predictFuture(bestMoveArray, currentY, currentX);
 		System.err.println(bestMove);
 		return bestMove;
 	}
 
 	private String followOrder(final int choose) {
-	    String order;
+		String order;
 		switch (choose) {
 		case 0:
-			order=this.GoUp();
+			order = this.GoUp();
 			break;
 		case 1:
-			order=this.GoDown();
+			order = this.GoDown();
 			break;
 		case 2:
-			order=this.GoLeft();
+			order = this.GoLeft();
 			break;
 		case 3:
-			order=this.GoRight();
+			order = this.GoRight();
 			break;
 		default:
 			Random rand = new Random();
@@ -326,8 +331,8 @@ class GameOrder {
 class Player {
 
 	public static void main(String[] args) {
-		final int maxY = 20+2;
-		final int maxX = 30+2;
+		final int maxY = 20 + 2;
+		final int maxX = 30 + 2;
 		final GameOrder tronOrder = new GameOrder();
 		final GameMap tronMap = new GameMap(maxY, maxX);
 		final ArrayList<LightRunner> allPlayers = new ArrayList<LightRunner>();
@@ -343,10 +348,10 @@ class Player {
 				}
 			}
 			for (int cptLightRunner = 0; cptLightRunner < nbLightRunner; cptLightRunner++) {
-				int beginningX = in.nextInt()+1;
-				int beginningY = in.nextInt()+1;
-				int currentX = in.nextInt()+1;
-				int currentY = in.nextInt()+1;
+				int beginningX = in.nextInt() + 1;
+				int beginningY = in.nextInt() + 1;
+				int currentX = in.nextInt() + 1;
+				int currentY = in.nextInt() + 1;
 				allPlayers.get(cptLightRunner).setBeginningX(beginningX);
 				allPlayers.get(cptLightRunner).setBeginningY(beginningY);
 				allPlayers.get(cptLightRunner).setCurrentX(currentX);
